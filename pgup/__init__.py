f60819d9488ae84a952e4eacae54c7da63ea7644
Stepan Perlov <stepanperlov@gmail.com>
2015-04-03 21:13:36 +0300

    Refactoring init logic

--- pgup/__init__.py
+++ pgup/__init__.py
@@ -2,5 +2,5 @@
 __version__ = "0.2.3"
 from config import Config
 from diff import diff
-from init import init
+from init import build_init
 from counter import Counter
--- pgup/init.py
+++ pgup/init.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
+import re
 import io
+import logging
+import collections
+
 import yaml
+from pake.shell import mkdir
 from table import Table
 from errors import PgupException
+from counter import Counter
 
-def get_sql_from_structure(structure):
+logger = logging.getLogger("pgup")
+
+def parse_structure(structure_path):
+    with open(structure_path) as fstream:
+        structure = yaml.load(fstream)
+
+    created = collections.defaultdict(int)
+
     queries = []
     names = []
     for obj in structure["modules"]:
@@ -23,24 +36,48 @@ def get_sql_from_structure(structure):
             with io.open(value, "r", encoding="utf-8") as fstream:
                 queries.append(fstream.read())
         names.append(name)
-    return queries, names
-
-
-def init(gui_db_structure_path, cached_db_structure_path, config):
-    with open(gui_db_structure_path) as fstream:
-        gui_db_structure = yaml.load(fstream)
-
-    queries, names = {}, {}
-    queries["gui_db"], names["gui_db"] = get_sql_from_structure(gui_db_structure)
-
-    if cached_db_structure_path and cached_db_structure_path != "gui_db":
-        with open(cached_db_structure_path) as fstream:
-            cached_db_structure = yaml.load(fstream)
-        queries["cached_db"], names["cached_db"] = get_sql_from_structure(cached_db_structure)
-
-    res = {
-        "overview": Table.overview(),
+        created[name] += 1
+    return {
         "queries": queries,
-        "names": names
+        "names": names,
+        "overview": u" / ".join(
+            [u"{} {}".format(name, count) for name, count in created.iteritems()]
+        )
     }
-    return res
+
+def build_init(args, argv, structures, pgup_config):
+    data = []
+    for dbname, param in structures:
+        if argv[param]:
+            structure_path = argv[param]
+            data.append( (dbname, parse_structure(structure_path)) )
+
+    for dbname, dbdata in data:
+        if dbdata["queries"]:
+            DBDIR = u"{}/{}".format(args.build, dbname)
+            DBFILES = u"{}/sql".format(DBDIR)
+            mkdir("-p {}".format(DBFILES))
+            files = []
+            counter = Counter()
+            # save prepared sql
+            for qry, name in zip(dbdata["queries"], dbdata["names"]):
+                counter.next()
+                fpath = u"{}/{}:{}-{}.sql".format(
+                    DBFILES,
+                    counter.get_textnum(),
+                    counter.get_intnum(),
+                    name
+                )
+                with io.open(fpath, "w", encoding="utf-8") as fstream:
+                    fstream.write(qry)
+                files.append(u"\\i '{}';".format(fpath))
+            # main file, which execute all saved sql
+            with io.open(u"{}/execute.sql".format(DBDIR), "w", encoding="utf-8") as fstream:
+                fstream.write(u"\n".join(files))
+            # overview about created objects
+            with io.open(u"{}/overview.txt".format(DBDIR), "w", encoding="utf-8") as fstream:
+                fstream.write(dbdata["overview"])
+
+            logger.info("{}: {}".format(dbname, DBDIR))
+        else:
+            logger.info("{}: Queries not exists".format(dbname))
