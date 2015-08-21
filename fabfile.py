8ac20f5bec2d94e71b92e48568d18fd74be4e5d4
Stepan Perlov <stepanperlov@gmail.com>
2015-08-21 14:12:17 +0300

    Add fab task for syncing pgup with mezzo

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import task, local
+from fabric.api import task, local, execute
 
 @task
 def up():
@@ -9,6 +9,7 @@ def up():
     local("rm -rf pgup.egg-info")
     local("python ./setup.py sdist")
     local("twine upload dist/{}".format(local("ls dist", capture=True).strip()))
+    execute(syncMezzo)
 
 @task
 def docs():
@@ -18,3 +19,13 @@ def docs():
     local("rm -rf build/html")
     local("python ./setup.py build_sphinx")
     local("python ./setup.py upload_sphinx")
+
+@task
+def syncMezzo():
+    """
+        Copy current module version to mezzo project
+    """
+    local("rm -rf /opt/mezzo/pgup")
+    local("mkdir /opt/mezzo/pgup")
+    local("cp -R etc pgup /opt/mezzo/pgup")
+    local("cp LICENSE MANIFEST.in README.md setup.py /opt/mezzo/pgup")
